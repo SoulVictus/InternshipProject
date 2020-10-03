373890b53ae549ea3d4ecae7784836b0dbb4d81d
SoulVictus <darkwilk@gmail.com>
2020-10-03 20:55:23 +0200

    frontend and backend connection fixed

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const PROXY_URL = 'http://cors-anywhere.herokuapp.com/';
+  //remember to change ngrok every restart
+  const NGROK_URL = 'http://cb57bb83bbdb.ngrok.io/';
+  useEffect(() => {
+    fetch(PROXY_URL+NGROK_URL+"files/1")
+      .then(res => res.json())
+      .then((result) => {
+        console.log(result);
+        setItems(result);
+      },
+      (error) => {
+        console.log(error);
+      })
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ flex: 1, padding: 24 }}>
     </div>
   );
 }
+
 
 export default App;
